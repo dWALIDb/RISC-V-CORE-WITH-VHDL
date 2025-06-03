07f3410b7f7abde8a9a2382bea361a975d5ff831
dwalidb <brazzawalidmokrane@gmail.com>
2025-06-03 17:34:25 +0200

    Update flipper.c

--- setup_for_riscv_gcc/flipper.c
+++ setup_for_riscv_gcc/flipper.c
@@ -26,23 +26,23 @@ int main(){
     FILE* fw0=fopen("C:\\Users\\DELL\\Desktop\\xpack_RISCV_gcc\\mif_0.mif","w");
     FILE* fw=fopen("C:\\Users\\DELL\\Desktop\\xpack_RISCV_gcc\\instructions.txt","w");
     
-    sprintf(line,"DEPTH=256;\nWIDTH=8;\nADDRESS_RADIX=UNS;\nDATA_RADIX=HEX;\nCONTENT BEGIN\n[0..255] : 00;\n");
-    fwrite(line,1,84,fw0);
+    sprintf(line,"DEPTH=1024;\nWIDTH=32;\nADDRESS_RADIX=UNS;\nDATA_RADIX=HEX;\nCONTENT BEGIN\n[0..1023] : 00000000;\n");
+    fwrite(line,1,93,fw0);
     line[0]=0;
-    for (int i = 0; i < size; i+=1,pc+=1)
+    for (int i = 0; i < size; i+=4,pc+=1)
     {
-        sprintf(line,"%d : %02x ;\n",pc,a[i]);
+        sprintf(line,"%d : %02x%02x%02x%02x ;\n",pc,a[i+3],a[i+2],a[i+1],a[i+0]);
         fwrite(line,1,strlen(line),fw0);
         
-        for (int j = i; (j-i) < 4; j++)
-        {            
-            for (int k = 0; k < 8; k++)
-            { 
-                sprintf(line,"%x",(((a[j]<<k)& 0x80)>>7));
-                fwrite(line,1,1,fw);
-            }
-            fwrite("\n",1,1,fw);   
-        }
+        // for (int j = i; (j-i) < 4; j++)
+        // {            
+        //     for (int k = 0; k < 8; k++)
+        //     { 
+        //         sprintf(line,"%x",(((a[j]<<k)& 0x80)>>7));
+        //         fwrite(line,1,1,fw);
+        //     }
+        //     fwrite("\n",1,1,fw);   
+        // }
         // printf("%02x %02x %02x %02x\n",a[i+3],a[i+2],a[i+1],a[i+0]);    
         line[0]=0;
     }
