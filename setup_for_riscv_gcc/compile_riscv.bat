15b99ba5e7fbe34ff8c8c70fdcd58dc7b759aa27
dwalidb <brazzawalidmokrane@gmail.com>
2025-08-14 01:24:06 +0200

    Delete setup_for_riscv_gcc/compile_riscv.bat

--- setup_for_riscv_gcc/compile_riscv.bat
+++
@@ -1,4 +0,0 @@
-riscv-none-elf-gcc -T newlinker.ld -o newfirst.elf first.c -O1 -march=rv32imf -nostartfiles -nostdlib
-riscv-none-elf-objcopy -O binary newfirst.elf newfirst.bin 
-riscv-none-elf-objdump -d newfirst.elf > newfirst.asm
-flipper.exe
